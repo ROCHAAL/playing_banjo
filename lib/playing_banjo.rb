8df0049b14c9889183d9e9403feba6aa8bbc54d1
Patricia <prmonday@gmail.com>
2020-11-02 21:10:15 +0000

    refactoring the first elsif cover the case of Roberta

--- lib/playing_banjo.rb
+++ lib/playing_banjo.rb
@@ -1,7 +1,5 @@
 def playing_banjo(name)
-  if  name == "Roberta"
-    return "you are playing banjo!"
-  elsif name[0] == "R" || name[0] == "r"
+  if  name[0] == "R" || name[0] == "r"
     return "#{name} you are playing banjo!"
   elsif name == "Sara"
       return "#{name} you are not playing banjo!"
--- spec/playing_banjo_spec.rb
+++ spec/playing_banjo_spec.rb
@@ -1,16 +1,13 @@
 require 'playing_banjo'
 
 describe 'playing_banjo' do
-  it 'takes a name with the letter "R" or "r" in lower case and return a string ' do
-    game = playing_banjo('Roberta')
-  expect(game).to eq("you are playing banjo!")
-  end
   it 'takes a name with the letter "R" or "r" and return the name plus a sentence.'do
     game = playing_banjo('Ruan')
     expect(game).to eq ('Ruan you are playing banjo!')
   end
-  it 'takes a name with a letter that is not "R" or "r" and return the name plus a sentence.' do
-    game = playing_banjo('Sara')
-    expect(game).to eq('Sara you are not playing banjo!')
+
+      it 'takes a name with a letter that is not "R" or "r" and return the name plus a sentence.' do
+        game = playing_banjo('Sara')
+        expect(game).to eq('Sara you are not playing banjo!')
   end
 end
