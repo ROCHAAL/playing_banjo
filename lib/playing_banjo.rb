f831a3fc034967a9c48d24b889c7164c50b07fee
Patricia <prmonday@gmail.com>
2020-11-02 20:58:35 +0000

    playing_banjo written as a function

--- lib/playing_banjo.rb
+++ lib/playing_banjo.rb
@@ -1,12 +1,9 @@
-class PlayingBanjo
-
-  def getting_names(name)
-    if  name == "Roberta"
-      return "you are playing banjo!"
-    elsif name[0] == "R" || name[0] == "r"
-      return "#{name} you are playing banjo!"
-    elsif name == "Sara"
+def playing_banjo(name)
+  if  name == "Roberta"
+    return "you are playing banjo!"
+  elsif name[0] == "R" || name[0] == "r"
+    return "#{name} you are playing banjo!"
+  elsif name == "Sara"
       return "#{name} you are not playing banjo!"
-    end
   end
 end
--- spec/playing_banjo_spec.rb
+++ spec/playing_banjo_spec.rb
@@ -1,19 +1,16 @@
 require 'playing_banjo'
 
-describe 'PlayBanjo' do
+describe 'playing_banjo' do
   it 'takes a name with the letter "R" or "r" in lower case and return a string ' do
-    playing_banjo = PlayingBanjo.new
-    result = playing_banjo.getting_names('Roberta')
-  expect(result).to eq("you are playing banjo!")
+    game = playing_banjo('Roberta')
+  expect(game).to eq("you are playing banjo!")
   end
   it 'takes a name with the letter "R" or "r" and return the name plus a sentence.'do
-    playing_banjo = PlayingBanjo.new
-    result = playing_banjo.getting_names('Ruan')
-    expect(result).to eq ('Ruan you are playing banjo!')
+    game = playing_banjo('Ruan')
+    expect(game).to eq ('Ruan you are playing banjo!')
   end
   it 'takes a name with a letter that is not "R" or "r" and return the name plus a sentence.' do
-    playing_banjo = PlayingBanjo.new
-    result = playing_banjo.getting_names('Sara')
-    expect(result).to eq('Sara you are not playing banjo!')
+    game = playing_banjo('Sara')
+    expect(game).to eq('Sara you are not playing banjo!')
   end
 end
