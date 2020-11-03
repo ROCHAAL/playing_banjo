4814f945e1521784cecdaf2422f3a5ed3557fe93
Patricia <prmonday@gmail.com>
2020-11-03 14:23:27 +0000

    test case for "r"

--- spec/playing_banjo_spec.rb
+++ spec/playing_banjo_spec.rb
@@ -1,7 +1,7 @@
 require 'playing_banjo'
 
 describe 'playing_banjo' do
-    it 'takes a name with the letter "R" or "r" and return the name plus a sentence.'do
+    it 'takes a name with the letter "R"  and return the name plus a sentence.'do
       game = playing_banjo('Ruan')
       expect(game).to eq ('Ruan you are playing banjo!')
   end
@@ -9,5 +9,10 @@ describe 'playing_banjo' do
     it 'takes a name with a letter that is not "R" or "r" and return the name plus a sentence.' do
         game = playing_banjo('Sara')
     expect(game).to eq('Sara you are not playing banjo!')
+  end
+
+  it 'takes a name with the letter "r" and return the name plus a sentence.' do
+      game = playing_banjo('roger')
+    expect(game).to eq('roger you are playing banjo!')
   end
 end
