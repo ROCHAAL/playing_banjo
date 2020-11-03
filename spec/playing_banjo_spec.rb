0cf33ad82dd351d1ba8ab77dcc310864d3f8495e
Patricia <prmonday@gmail.com>
2020-11-03 09:54:00 +0000

    kata defined as a function,refactored code,code works for all names not beginning with R or r

--- spec/playing_banjo_spec.rb
+++ spec/playing_banjo_spec.rb
@@ -1,11 +1,12 @@
 require 'playing_banjo'
 
 describe 'playing_banjo' do
-  it 'takes a name with the letter "R" or "r" and return the name plus a sentence.'do
-    game = playing_banjo('Ruan')
-    expect(game).to eq ('Ruan you are playing banjo!')
+    it 'takes a name with the letter "R" or "r" and return the name plus a sentence.'do
+      game = playing_banjo('Ruan')
+      expect(game).to eq ('Ruan you are playing banjo!')
   end
-      it 'takes a name with a letter that is not "R" or "r" and return the name plus a sentence.' do
+
+    it 'takes a name with a letter that is not "R" or "r" and return the name plus a sentence.' do
         game = playing_banjo('Sara')
     expect(game).to eq('Sara you are not playing banjo!')
   end
